142374ad330b8b0937f9fcdafd9bdc6c9c5b6a1d
Gaspard Francony <GFR3788@viseo.net>
2018-06-22 16:59:30 +0200

    Add tests for router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
-const routes: Routes = [
+export const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'heroes', component: HeroesComponent },
   { path: 'detail/:id', component: HeroDetailComponent },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,53 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, tick, fakeAsync } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Routes, Router } from '@angular/router';
+import { routes } from './app-routing.module';
+import { AppModule } from './app.module';
+import { APP_BASE_HREF } from '@angular/common';
+
+describe('Router', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule.withRoutes(routes), AppModule],
+      providers: [
+        { provide: APP_BASE_HREF, useValue: '/' }
+      ]
+    });
+  });
+
+  it('can navigate to main (async)', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    TestBed.get(Router)
+      .navigate([''])
+      .then(() => {
+        expect(location.pathname).toEqual('/dashboard');
+      })
+      .catch(e => console.log(e));
+  }));
+
+  it('can navigate to dashboard (async)', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    TestBed.get(Router)
+      .navigate(['/dashboard'])
+      .then(() => {
+        expect(location.pathname).toEqual('/dashboard');
+      })
+      .catch(e => console.log(e));
+  }));
+
+  it('can navigate to heroes (async)', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    TestBed.get(Router)
+      .navigate(['/heroes'])
+      .then(() => {
+        expect(location.pathname).toEqual('/heroes');
+      })
+      .catch(e => console.log(e));
+  }));
+});
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -17,16 +64,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  xit(`should have as title 'app'`, async(() => {
+  it(`should have as title 'Tour of Heroes'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('Tour of Heroes');
   }));
 
-  xit('should render title in a h1 tag', async(() => {
+  it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Tour of Heroes');
   }));
 });
